de749001d6defd3bb441fc8bb5a21e76ed683fcf
Akhil T V <140319506+Akhil-0190@users.noreply.github.com>
2025-03-09 17:34:17 +0530

    Update snowflake_config.py

--- snowflake_config.py
+++ snowflake_config.py
@@ -3,10 +3,10 @@ import snowflake.connector
 # Function to create a Snowflake connection
 def get_connection():
     return snowflake.connector.connect(
-        user='Akhil019',
-        password='Snowflake@1234',
-        account='RLWGPKG-BG25603',
-        database='RESOURCE_DB',
-        schema='PUBLIC'
+        user='',
+        password='',
+        account='',
+        database='',
+        schema=''
     )
 
